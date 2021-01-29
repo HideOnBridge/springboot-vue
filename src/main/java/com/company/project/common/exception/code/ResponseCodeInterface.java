1def4a2ee2342693ef021ebd929c36edbf61b4c4
“HideOnBridge” <2669940936@qq.com>
2021-01-29 09:53:09 +0800

    version 1.11111

---
+++ src/main/java/com/company/project/common/exception/code/ResponseCodeInterface.java
@@ -0,0 +1,24 @@
+package com.company.project.common.exception.code;
+
+/**
+ * ResponseCodeInterface
+ *
+ * @author
+ * @version
+ * @date
+ */
+public interface ResponseCodeInterface {
+    /**
+     * 获取code
+     *
+     * @return code
+     */
+    int getCode();
+
+    /**
+     * 获取信息
+     *
+     * @return msg
+     */
+    String getMsg();
+}
---
+++ src/main/java/com/company/project/controller/KaptchaController.java
@@ -0,0 +1,44 @@
+package com.company.project.controller;
+
+import com.wf.captcha.ArithmeticCaptcha;
+import com.wf.captcha.utils.CaptchaUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 验证码相关
+ *
+ * @author mc
+ * @version
+ * @date 2020年3月18日
+ */
+@Api(tags = "验证码相关")
+@RestController
+@Slf4j
+@RequestMapping("/sys")
+public class KaptchaController {
+    /**
+     * 获取验证码图片
+     * Gets captcha code.
+     *
+     * @param request  the request
+     * @param response the response
+     * @throws IOException the io exception
+     */
+    @GetMapping("/getVerify")
+    @ApiOperation(value = "获取验证码",notes = "不需要权限")
+    public void getCaptchaCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ArithmeticCaptcha captcha = new ArithmeticCaptcha(130, 48);
+        captcha.setLen(2);
+        CaptchaUtil.out(captcha, request, response);
+    }
+
+}
